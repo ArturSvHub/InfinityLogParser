a45e93db93f0fbceac5f49b9efeb2a8f7baa98a9
Artur Sveshnikov <73856281+ArturSvHub@users.noreply.github.com>
2022-12-16 06:29:54 +0300

    alpha 0.0.0.1 start

---
+++ Parser/Call.cs
@@ -0,0 +1,13 @@
+﻿namespace Parser
+{
+    public class Call
+    {
+        public string? OnIncomingCallDate { get; set; }
+        public int? CalledNumber { get;  set; }
+        public int? Connection { get;  set; }
+        public int? Uid { get;  set; }
+        public string? CallerName { get;  set; }
+        public string? CallerAddress { get;  set; }
+        public string? CallerNumber { get;  set; }
+    }
+}
